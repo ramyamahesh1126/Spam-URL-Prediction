8e9e20218902226f7c566957f6c882a47c0bbe4e
ramyamahesh1126 <89604840+ramyamahesh1126@users.noreply.github.com>
2021-11-28 21:53:33 -1000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import uvicorn
 import pickle
 import sklearn
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from feature import feature_extraction
 
 with open('model1.pickle','rb') as f:
@@ -14,8 +14,9 @@ def index():
     return {'message': 'Detecting Spam URL'}
 
 @app.get('/predict/')
-def predict(url: str):
+def predict(url: str, response: Response):
     df=feature_extraction(url)
+    response.headers['Access-Control-Allow-Origin']= "*"
     return {'output': model.predict(df).tolist()}
 
 if __name__ == '__main__':
